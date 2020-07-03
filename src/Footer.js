5cff16bcd982e4d592f29d4d022f6767c8e434eb
Ayanok0uji <FlowAkashi.@ya.ru>
2020-07-04 03:01:56 +0500

    commit 4

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,35 @@ const Footer = () => {
 
     return (
         <div>
-
+            <h2>Get in touch</h2>
+            <p>Praesent dapibus, neque id cursus faucibus, tortor neque ege tas augue, eu vulputate magna eros eu erat.
+            Aliquam erat et volutpat. Nam dui mi, tincidunt quis, accumsan porttitor tempus lorem ipsum dolor sit amet consequat.</p>
+            <ul className={'major-icons'}>
+                <li>
+                    <h3 className={'icon' + ' ' + 'solid' + ' ' + 'fa-phone' + ' ' + 'major'}>
+                        <span className={'label'}>Phone</span>
+                    </h3>
+                    <p>(000) 000-0000</p>
+                </li>
+                <li>
+                    <h3 className={'icon' + ' ' + 'solid' + ' ' + 'fa-map' + ' ' + 'major'}>
+                        <span className={'label'}>Map</span>
+                    </h3>
+                    <p>1234 Fictional Road
+                    <br />
+                    Nashville, TN 00000
+                    </p>
+                </li>
+                <li>
+                    <h3 className={'icon' + ' ' + 'solid' + ' ' + 'fa-envelope' + ' ' + 'major'}>
+                        <span className={'label'}>Email</span>
+                    </h3>
+                    <p><a href={'#'}>info@untitled.tld</a></p>
+                </li>
+                
+            </ul>
+            <ul className={'joined-icons'}></ul>
+            <p className={'copyright'}>© Untitled. All rights reserved. Lorem ipsum dolor sit amet.</p>
         </div>
     )
 
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Main.css'
+import contain from './contain.jpg'
+import pic01 from './pic05.jpg'
+import pic02 from './pic06.jpg'
+import pic03 from './pic07.jpg'
 
 
 
@@ -28,9 +32,128 @@ const Main = () => {
                     </li>
                 </ul>
             </section>
-            <section></section>
-            <section></section>
+            <section id={'one'} className={'wrapper' + ' ' + 'spotlight'}>
+                <div className={'content'}>
+                    <div className={'inner'}>
+                        <h2>Magna veroeros</h2>
+                        <p>
+                        Praesent dapibus, neque id cursus faucibus, tortor neque ege tas augue, eu vulputate magna eros eu erat.
+                         Aliquam erat et volutpat. Nam dui mi, tincidunt quis, accumsan porttitor tempus.
+                        </p>
+                        <ul className={'action'}>
+                            <li><a className={'button'}>Learn More</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <ul className={'grid-icons'}>
+                    <li>
+                        <div className={'inner'}>
+                            <span className={'icon' + ' ' + 'solid' + ' ' + 'fa-cog' + ' ' + 'major'}></span>
+                            <h3>Sed tempus</h3>
+                        </div>
+                    </li>
+                    <li>
+                        <div className={'inner'}>
+                            <span className={'icon' + ' ' + 'solid' + ' ' + 'fa-desktop' + ' ' + 'major'}></span>
+                            <h3>Aenean lorem</h3>
+                        </div>
+                    </li>
+                    <li>
+                        <div className={'inner'}>
+                            <span className={'icon' +  ' ' + 'solid' + ' ' + 'fa-signal' + ' ' + 'major'}></span>
+                            <h3>Felis aliquam</h3>
+                        </div>
+                    </li>
+                    <li>
+                        <div className={'inner'}>
+                            <span className={'icon' +  ' ' + 'solid' + ' ' + 'fa-check' + ' ' + 'major'}></span>
+                            <h3>Dui volutpat</h3>
+                        </div>
+                    </li>
+                    
+                </ul>
+            </section>
+            <section id={'two'} className={'wrapper' + ' ' + 'spotlight' + ' ' + 'style1' + ' ' + 'alt'}>
+                <div className={'content'}>
+                <div className={'inner'}>
+                        <h2>Ipsum accumsan</h2>
+                        <p>
+                        Praesent dapibus, neque id cursus faucibus, tortor neque ege tas augue, eu vulputate magna eros eu erat.
+                         Aliquam erat et volutpat. Nam dui mi, tincidunt quis, accumsan porttitor tempus.
+                        </p>
+                        <ul className={'action'}>
+                            <li><a className={'button'}>Learn More</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={'image' + ' ' + 'n1'}>
+                    <img src={contain}/>
+                </div>
+            </section>
+            <section id={'three'} className={'wrapper' + ' ' + 'spotlight' + ' ' + 'style2'}>
+                <div className={'content'}>
+                <div className={'inner'}>
+                        <h2>Etiam consequat</h2>
+                        <p>
+                        Praesent dapibus, neque id cursus faucibus, tortor neque ege tas augue, eu vulputate magna eros eu erat.
+                         Aliquam erat et volutpat. Nam dui mi, tincidunt quis, accumsan porttitor tempus.
+                        </p>
+                        <ul className={'action'}>
+                            <li><a className={'button'}>Learn More</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={'image' + ' ' + 'n2'}>
+                    <img src={contain}/>
+                </div>
+            </section>
+            <section id={'four'} className={'wrapper' + ' ' + 'spotlight' + ' ' + 'style3'}>
+                <div className={'content'}>
+                <div className={'inner'}>
+                        <h2>Etiam consequat</h2>
+                        <p>
+                        Praesent dapibus, neque id cursus faucibus, tortor neque ege tas augue, eu vulputate magna eros eu erat.
+                         Aliquam erat et volutpat. Nam dui mi, tincidunt quis, accumsan porttitor tempus.
+                        </p>
+                        <ul className={'action'}>
+                            <li><a className={'button'}>Learn More</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={'image' + ' ' + 'n3'}>
+                    <img src={contain}/>
+                </div>
+            </section>
+            <section id={'five'} className={'wrapper' + ' ' + 'special'}>
+                <h2>Accumsan praesent</h2>
+                <p>Praesent dapibus, neque id cursus faucibus, tortor neque ege tas augue, eu vulputate magna eros eu erat.
+                 Aliquam erat et volutpat. Nam dui mi, tincidunt quis, accumsan porttitor tempus lorem ipsum dolor sit amet consequat.</p>
+                <ul className={'faces'}>
+                    <li>
+                        <span className={'images'}>
+                            <img src={pic01}/>
+                        </span>
+                        <h3>Jane Doe</h3>
+                        <p>"Aliquam erat et volutpat. Nam dui mi, tincidunt quis, porttitor magna etiam lorem tempus."</p>
+                    </li>
+                    <li>
+                        <span className={'images'}>
+                            <img src={pic02}/>
+                        </span>
+                        <h3>John Anderson</h3>
+                        <p>"Aliquam erat et volutpat. Nam dui mi, tincidunt quis, porttitor magna etiam lorem tempus."</p>
+                    </li>
+                    <li>
+                        <span className={'images'}>
+                            <img src={pic03}/>
+                        </span>
+                        <h3>Kate Smith</h3>
+                        <p>"Aliquam erat et volutpat. Nam dui mi, tincidunt quis, porttitor magna etiam lorem tempus."</p>
+                    </li>
+                </ul>
+            </section>
         </div>
+
     )
 
 }
